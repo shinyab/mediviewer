ae9283d14dc099b9f1975cc5227bded3a48055f8
Daesub Kim <rano@bluehack.net>
2017-11-08 20:45:03 +0900

    Adding the SegmentationPopup view in progress

--- src/components/layout/AppContents.vue
+++ src/components/layout/AppContents.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="app-main">
     <router-view></router-view>
-    <!--<vue-draggable-resizable :parent="true" style="border-radius: 5px; background-color: #282828; z-index: 2000;">-->
-      <!--<div style="margin-top:40px; width:100%; height: 80%; background-color: #0d0c0f">-->
-        <!--<ul style="height: 100%">-->
-          <!--<li v-for="n in 10" style="cursor: crosshair;">-->
-            <!--awefwaefawef-->
-          <!--</li>-->
-        <!--</ul>-->
-      <!--</div>-->
-    <!--</vue-draggable-resizable>-->
+    <!--<segmentation-popup></segmentation-popup>-->
   </div>
 </template>
 
 <script>
+  import SegmentationPopup from '@/components/popups/SegmentationPopup'
+
   export default {
-    name: 'AppContents'
+    name: 'AppContents',
+    components: {
+      SegmentationPopup
+    }
   }
 </script>
 
---
+++ src/components/popups/SegmentationPopup.vue
@@ -0,0 +1,25 @@
+<template>
+  <vue-draggable-resizable
+    :parent="true"
+    :x="0" :y="80" :z="2000"
+    :w="350" :h="400"
+    :minw="350" :minh="400">
+    <div style="margin-top:40px; width:100%; height: 80%; background-color: #0d0c0f">
+      <ul style="height: 100%">
+        <li v-for="n in 10" style="cursor: crosshair;">
+          awefwaefawef
+        </li>
+      </ul>
+    </div>
+  </vue-draggable-resizable>
+</template>
+
+<script>
+  export default {
+    name: 'SegmentationPopup'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
