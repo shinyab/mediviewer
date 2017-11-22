0501169ed10151c905154fda681f108239b76b75
신승영 <shinyab@bluehack.net>
2017-11-22 11:41:04 +0900

    add comment for Medic3D path

--- src/lib/medic3d/index.js
+++ src/lib/medic3d/index.js
@@ -1,6 +1,7 @@
 import JSZIP from 'jszip';
 import * as THREE from 'three';
 import Medic3D from '../../../../Medic3D/dist/medic3d'
+// for release
 // import Medic3D from '../../../static/lib/Medic3D/medic3d'
 import Request from 'request';
 var PNG = require('pngjs').PNG;
